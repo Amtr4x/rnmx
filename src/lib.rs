4104798c3880a532b05082bfc919093a7ac43bb9
leandryscsec <leandryscsec@gmail.com>
2024-06-21 18:02:12 -0400

    ADD library functions placeholders

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
 //! RNM tool definitions and test
 //! author: @Amtr4x
+
+use std::io::Result;
+
+pub struct Asset<'a> {
+    asset_type: &'a str,
+    path: String,
+}
+
+impl<'a> Asset<'a> {
+    // asset_type can be identified by the path structure checking for / at the last char of the path
+    pub fn new(path: String) -> Self {
+        Self {
+            asset_type: "",
+            path,
+        }
+    }
+
+    pub fn rename(&self, new_path: String) -> Result<u8> {
+        Ok(0)
+    }
+}
