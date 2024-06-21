29a1e1d4a0e7722efdd67127746e2f2de0a2eff5
leandryscsec <leandryscsec@gmail.com>
2024-06-21 18:22:45 -0400

    ADD display banner function which is part of display menu function

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,24 @@
 //! author: @Amtr4x
 //! license: MIT
 fn main() {}
+
+fn _display_banner() {
+    let tool_name: &str = "RENAME TOOL";
+    let padding: String = " ".repeat(4);
+
+    println!("{padding}{}", "#".repeat(tool_name.len() + 8));
+    println!(
+        "{padding}{}{}{}",
+        "#".repeat(2),
+        " ".repeat(tool_name.len() + 4),
+        "#".repeat(2)
+    );
+    println!("{padding}{}  {tool_name}  {}", "#".repeat(2), "#".repeat(2));
+    println!(
+        "{padding}{}{}{}",
+        "#".repeat(2),
+        " ".repeat(tool_name.len() + 4),
+        "#".repeat(2)
+    );
+    println!("{padding}{}\n", "#".repeat(tool_name.len() + 8));
+}
