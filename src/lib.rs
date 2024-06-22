eb981a34e24f08d574b9f99bf4caba98c46bcd14
leandryscsec <leandryscsec@gmail.com>
2024-06-22 13:28:46 -0400

    UPDATE rename function and UPDATE new function of asset type

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 use std::{
     fs,
     io::{self},
-    path::Path,
 };
 
 pub struct Asset(String);
@@ -14,29 +13,52 @@ impl Asset {
         Self(path)
     }
 
+    /// Rename the provided asset. First checks for a file and
+    /// if not a file found checks for a directory.
+    ///
+    /// ## Arguments
+    /// * `&self` - self asset object reference.
+    /// * `new_path` - the new named asset, should not be empty.
+    ///
+    /// ## Panics
+    /// If an empty string name is provided function will panic.
+    /// If you try to rename a directory with file path or viceversa.
+    ///
+    /// ## Example
+    /// ```rust
+    /// use rnm::Asset;
+    ///
+    /// // Simulate creating a new asset instance for a file
+    /// let file_path = Asset::new(String::from("hello.txt"));
+    ///
+    /// file_path.rename(String::from("bye.txt"));
+    ///
+    ///```
     pub fn rename(&self, new_path: String) {
-        let path: &Path = Path::new(&self.0);
-
-        if Path::is_file(path) {
-            rename_file(&self.0, new_path).unwrap_or_else(|err| {
+        if !self.0.ends_with('/') && !new_path.ends_with('/') {
+            rename_file(&self.0, &new_path).unwrap_or_else(|err| {
                 eprintln!("{err}");
             });
         } else {
-            rename_dir(&self.0, new_path).unwrap_or_else(|err| {
+            rename_dir(&self.0, &new_path).unwrap_or_else(|err| {
                 eprintln!("{err}");
             });
         }
     }
 }
 
-fn rename_file(path: &String, new_path: String) -> io::Result<()> {
-    assert!(Path::is_file(Path::new(&path)) && !new_path.ends_with('/'));
-    fs::rename(path, new_path)?;
+fn rename_file(path: &String, new_path: &String) -> io::Result<()> {
+    if !path.ends_with('/') && !new_path.ends_with('/') {
+        fs::rename(path, new_path)?;
+        println!("File renamed successfuly");
+    }
     Ok(())
 }
 
-fn rename_dir(path: &String, new_path: String) -> io::Result<()> {
-    assert!(Path::is_dir(Path::new(&path)) && new_path.ends_with('/'));
-    fs::rename(path, new_path)?;
+fn rename_dir(path: &String, new_path: &String) -> io::Result<()> {
+    if path.ends_with('/') && new_path.ends_with('/') {
+        fs::rename(path, new_path)?;
+        println!("Directory renamed successfuly");
+    }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! CLI tool to rename files and folders in Windows, Linux and MacOS.
 //! author: @Amtr4x
 //! license: MIT
+
 fn main() {}
 
 fn display_banner() {
